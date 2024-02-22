98d75e3f3e7793dcd0ddcb0a8d81388887a7ef8d
korol <korol@DESKTOP-6AQ5CC1>
2024-02-22 18:22:42 +0200

    Add 'Film' class
    Create three objects for 'Film' class

---
+++ Database Film/Film.cpp
@@ -0,0 +1,9 @@
+#include "Film.h"
+
+void Film::printInfo()
+{
+
+	cout << "Title: " << name << "\nYear: " << year << "\nDirector: " << director << "\nGenre: " << genre << endl;
+	cout << "-------------" << endl;
+
+}
---
+++ Database Film/Film.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Film
+{
+
+private:
+
+    int year;
+    string name;
+    string  director;
+    string genre;
+
+public:
+
+    Film(string n) : name(n) {}
+    Film(string n, int y) : name(n), year(y) {}
+    Film(string n, int y, string g) : name(n), year(y), genre(g) {}
+    Film(string n, int y, string g, string d) : name(n), year(y), genre(g), director(d) {}
+
+    ~Film() {}
+
+    void printInfo();
+
+};
