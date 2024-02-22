b25635a76d8f983a5b82c0ac4aa810762bc180a3
Vlad Korolenko <korolenkovlad0@gmail.com>
2024-02-22 19:41:21 +0200

    Add new objects for 'Film' and 'Review' class. Add 2 new objects into 'baseFilms' class.

--- Database Film/Database Film.cpp
+++ Database Film/Database Film.cpp
@@ -15,12 +15,14 @@ int main()
     Film firstFilm("Oppenheimer", 2023, "Biographical thriller film", "C. Nolan");
     Film secondFilm("Interstellar", 2014, "Science fiction film", "C. Nolan");
     Film thirdFilm("Harry Potter", 2001, "Fantasy", "C. Columbus");
+    Film fourthFilm("Pirates of the Caribbean", 2003, "Fantasy", "T. Elliot");
 
     // Інформація про відгуки 
 
     Review firstReview("Oppenheimer", "vlkorolenko", 4.5, "20.02.2024");
     Review secondReview("Interstellar", "abcd", 5.0, "13.09.2016");
     Review thirdReview("Harry Potter", "potter124", 4.1, "02.10.2012");
+    Review fourthReview("Pirates of the Caribbean", "pirat38", 3.9, "07.10.2007");
 
     baseFilms baseFilms;    // Оголошення бази данних
 
@@ -29,6 +31,7 @@ int main()
     baseFilms.addFilmInfo(firstFilm, firstReview);
     baseFilms.addFilmInfo(secondFilm, secondReview);
     baseFilms.addFilmInfo(thirdFilm, thirdReview);
+    baseFilms.addFilmInfo(fourthFilm, fourthReview);
 
     baseFilms.printAllInfo();
 
